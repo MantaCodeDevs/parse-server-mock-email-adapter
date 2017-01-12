14e7942adbc27e12ce885c809f2502e6d3d60993
Akos Orban <akos.orban@gmail.com>
2017-01-12 22:38:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict"
+const nodemailer = require('nodemailer')
+const stubTransport = require('nodemailer-stub-transport')
+
+const stubMailer = nodemailer.createTransport(stubTransport())
+
+const MockParseEmailAdapter = (options) => {
+    const sendMail = (mail) => {
+        const mailOptions = {
+            from: options.fromAddress,
+            to: mail.to,
+            html: mail.text,
+            subject: mail.subject,
+        }
+
+        return new Promise((resolve, reject) => {
+            stubMailer.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    reject(error)
+                } else {
+                    if (!!info && !!info.response) {
+                        console.log(info.response.toString())
+                    }
+
+                    resolve(info)
+                }
+            })
+        })
+    }
+
+    return {
+        sendMail: sendMail
+    }
+}
+
+module.exports = MockParseEmailAdapter
